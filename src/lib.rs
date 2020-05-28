8a28502e36925c1eb6debbfc0316366852e65e1b
garebare <garedaiki@outlook.jp>
2020-05-28 16:28:05 +0900

    add:ppu

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ pub mod ram;
 pub mod cpu;
 pub mod bus;
 pub mod helper;
+pub mod ppu;
---
+++ src/ppu/register/scroll.rs
@@ -0,0 +1,13 @@
+pub struct Scroll {
+  x: u8,
+  y: u8,
+}
+
+impl Scroll{
+  pub fn new() -> Scroll {
+    Scroll{
+      x: 0,
+      y: 0,
+    }
+  }
+}
