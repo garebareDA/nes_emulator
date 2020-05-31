667292cd0972150d2edde823d1beda2851c5edd6
garebare <garedaiki@outlook.jp>
2020-06-01 04:55:44 +0900

    fix

--- src/ppu/ppu.rs
+++ src/ppu/ppu.rs
@@ -1,5 +1,28 @@
 use super::register;
+use super::background;
+use super::super::cassette::roms::Rom;
+use super::super::ram;
 
 pub struct PPU {
+  pub cycle: usize,
+  pub line: usize,
   register: register::Registers,
+  background: background::Background,
+  cassette: Rom,
+  vram: ram::Ram,
+}
+
+impl PPU {
+  pub fn new (cassette: Rom) -> PPU {
+    PPU {
+      cycle: 0,
+      line: 0,
+      register: register::Registers::new(),
+      background: background::Background::new(),
+      cassette: cassette,
+      vram: ram::Ram::new(vec![0; 0x0200]),
+    }
+  }
+
+  
 }
