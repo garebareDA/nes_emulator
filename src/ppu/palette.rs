1e44b08b83b878ea9f1537e3fd315f8f92d32347
garebare <garedaiki@outlook.jp>
2020-06-02 22:58:06 +0900

    fix

--- src/ppu/palette.rs
+++ src/ppu/palette.rs
@@ -1,17 +1,36 @@
 #[derive(Debug, Clone)]
 pub struct PaletteList {
-  palette: Vec<u8>
+  palette: Vec<u8>,
 }
-
 
 impl PaletteList {
   pub fn new() -> PaletteList {
-    PaletteList{
-      palette:vec!(0; 0x20)
+    PaletteList {
+      palette: vec![0; 0x20],
     }
   }
 
-  pub fn pallet_get(self, palette_id: u8) ->  Vec<u8>{
+  pub fn read(&self, addr: u16) -> u8 {
+    if self.clone().is_sprite_mirror(addr) {
+      return self.palette[(addr - 0x10) as usize];
+    }
+
+    if self.clone().is_background_mirror(addr) {
+      return self.palette[0x00];
+    }
+
+    self.palette[addr as usize]
+  }
+
+  fn is_background_mirror(self, addr: u16) -> bool {
+    (addr == 0x04) || (addr == 0x08) || (addr == 0x0c)
+  }
+
+  fn is_sprite_mirror(self, addr: u16) -> bool {
+    (addr == 0x10) || (addr == 0x14) || (addr == 0x18) || (addr == 0x1c)
+  }
+
+  pub fn pallet_get(self, palette_id: u8) -> Vec<u8> {
     let offset = 0x00;
     let start = (palette_id * 4 + offset) as usize;
     let end = start + 4;
--- src/ppu/register/mod.rs
+++ src/ppu/register/mod.rs
@@ -1,16 +1,20 @@
+use super::super::ram;
+use super::palette::PaletteList;
+use super::super::cassette::roms::Rom;
+
 pub struct Registers {
-  ctrl1 : u8,
-  ctrl2 : u8,
+  ctrl1: u8,
+  ctrl2: u8,
   status: u8,
-  sprite_addr : u8,
-  sprite_data : u16,
-  ppu_addr : u16,
+  sprite_addr: u8,
+  sprite_data: u16,
+  ppu_addr: u16,
   ppu_data: u8,
 }
 
 impl Registers {
   pub fn new() -> Registers {
-    Registers{
+    Registers {
       ctrl1: 0,
       ctrl2: 0,
       status: 0,
@@ -20,4 +24,72 @@ impl Registers {
       ppu_data: 0,
     }
   }
+
+  fn read_status(&mut self) -> u8 {
+    let data = self.status;
+    self.clear_vblank();
+    self.clear_sprite_hit();
+    data
+  }
+
+  fn clear_vblank(&mut self) {
+    self.status &= 0x7F;
+  }
+
+  fn clear_sprite_hit(&mut self) {
+    self.status &= 0xbF;
+  }
+
+  pub fn get_backgound_table_offset(&self) -> u16 {
+    if self.ctrl1 & 0x10 == 0x10 {
+      0x1000
+    } else {
+      0x0000
+    }
+  }
+
+  pub fn get_sprite_table_offset(&self) -> u16 {
+    if self.ctrl1 & 0x08 == 0x08 {
+      0x1000
+    } else {
+      0x0000
+    }
+  }
+
+  pub fn is_background_enable(&self) -> bool {
+    self.ctrl2 & 0x08 == 0x08
+  }
+
+  pub fn read(&mut self, addr: u16, cassette: &Rom, vram:&mut ram::Ram, palette:&mut PaletteList) -> u8 {
+    match addr {
+      0x0002 => self.read_status(),
+      0x0004 => {println!("oam"); 0},
+      0x0007 => self.ppu_data_read(addr, cassette, vram, palette),
+      _ => 0,
+    }
+  }
+
+  fn ppu_data_read(&mut self, addr:u16, cassette: &Rom, vram:&mut ram::Ram, palette:&mut PaletteList) -> u8 {
+    let buf = self.ppu_data;
+    if addr >= 0x2000 {
+      let addr = self.calc_addr(addr);
+      if addr >= 0x3F00{
+        self.ppu_data = vram.read(addr);
+        return palette.read(addr - 0x3f00);
+      }
+      self.ppu_addr = vram.read(addr) as u16;
+    }else{
+      self.ppu_addr = cassette.rom_read(addr) as u16;
+    }
+
+    buf
+  }
+
+  fn calc_addr(&self, addr: u16) -> u16 {
+    if addr >= 0x3000 && addr < 0x3f00 {
+        addr - 0x3000
+    } else {
+        addr - 0x2000
+    }
+}
 }
