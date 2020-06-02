c71765f8452a9015f3781ac6da7aef4d6aa3eff9
garebare <garedaiki@outlook.jp>
2020-06-02 21:37:06 +0900

    remove:scroll

--- src/ppu/register/mod.rs
+++ src/ppu/register/mod.rs
@@ -1,12 +1,9 @@
-pub mod scroll;
-
 pub struct Registers {
   ctrl1 : u8,
   ctrl2 : u8,
   status: u8,
   sprite_addr : u8,
   sprite_data : u16,
-  scroll: scroll::Scroll,
   ppu_addr : u16,
   ppu_data: u8,
 }
@@ -19,7 +16,6 @@ impl Registers {
       status: 0,
       sprite_addr: 0,
       sprite_data: 0,
-      scroll: scroll::Scroll::new(),
       ppu_addr: 0,
       ppu_data: 0,
     }
